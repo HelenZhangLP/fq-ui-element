801801b50c15b0782075c73469ea2806499bcec5
zhangliping <zhangliping@ifunq.com>
2018-09-14 16:32:36 +0800

    first commit

---
+++ src/pages/index/layout/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <div class="title">单列布局 —— 整行显示，子元素水平垂直居中</div>
+    <div class="layout-block-wrap">
+      <img src="../../../images/app.png" alt="" class="son-element">
+      <div class="text text-align">fangqu ui frame</div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'index.vue'
+  }
+</script>
+
+<style scoped>
+
+</style>
